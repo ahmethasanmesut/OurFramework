b79112e8ad9db1013d5cf19f3a43afd6f2ab6eed
Hasosoon <hasanemirr15@gmail.com>
2022-04-06 13:48:24 +0100

    From scratch

---
+++ src/test/java/Runner/CukesRunner.java
@@ -0,0 +1,20 @@
+package Runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "json:target/cucumber.json",
+                "html:target/default-html-reports",
+                "rerun:target/rerun.txt"
+        },
+        features = "src/test/resources/features",
+        glue = "src/test/java/Step_Defs",
+        dryRun = true,
+        tags = "@wip"
+)
+public class CukesRunner {
+}
---
+++ src/test/java/pages/BasePage.java
@@ -0,0 +1,12 @@
+package pages;
+
+import Utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
+
+public abstract class BasePage {
+
+    public BasePage(){
+        PageFactory.initElements(Driver.get(),this);
+    }
+
+}
